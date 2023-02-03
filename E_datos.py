dc48051443c912280fa5fe8eecc4bae22befe2ab
Jeanpierre <jeanpierreloaizaleon@gmail.com>
2023-02-03 14:00:16 -0500

    Cambios y nuevos ejecicios

---
+++ E_datos.py
@@ -0,0 +1,17 @@
+# Para entrada de datos usamos input
+palabra = input("Introduce una palabra:  ")
+
+# para entrada de enteros
+num_int = int(input("Introduce un numero entero: "))
+
+# para entrada de flotantes
+num_float = float(input("Introduce un numero decimal(flotante): "))
+
+# para etrada de complejos
+num_compless = complex(input("Introduce un numero imaginario: "))
+
+
+print("String: ",palabra)
+print("Entero: ",num_int)
+print("Flotante: ",num_float)
+print("Numero complejo: ",num_compless)
---
+++ E_datos2.py
@@ -0,0 +1,11 @@
+# Entrada de datos ejemplo
+
+nombre = input("¿Cual es tu nombre? ")
+print("Hola" + nombre + ", vamos a realizar una suma.")
+
+num_uno = int(input("porfavor introduce el primer valor: "))
+num_dos = int(input("porfavor introduce el segundo valor: "))
+
+resultado = num_uno + num_dos 
+
+print(nombre + " El resultado de la suma es: ",resultado)
